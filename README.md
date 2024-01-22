28b251d97682ad8bc79010c81610469f24c2e1b6
Yaay Nands <16502598+nandhinianandj@users.noreply.github.com>
2024-01-22 22:49:22 +0530

    Life is one long,never-ending and tiring optimization problem.
    --Aang Jie

---
+++ README.md
@@ -0,0 +1,9 @@
+# Ikigai diagram creator
+## Based on the pyvenn package seen here with some slight modifications
+
+Checkout this repository first:
+```python
+git clone https://github.com/tctianchi/pyvenn.git
+cd pyvenn
+```
+
---
+++ ikigai_diagram.py
@@ -0,0 +1,28 @@
+import io
+import venn
+import matplotlib.pyplot as plt
+from collections import ChainMap
+
+
+values = [['Systems Design', 'Coding/SW_Engineering'],
+          ['Writing','Connecting With People','Play Bass Guitar', 'Teach Philosophy Alignment',
+              'Systems Design', 'Coding/SW_Engineering', 'Making People Laugh'],
+          ['Play badminton', 'Running', 'Reading', 'Dance', 'Making People Laugh'],
+          ['Write Poetry','Teach Philosophy Alignment', 'Systems Design', 'Connecting with people',
+           'Writing', 'Coding/SW_Engineering', 'Dance', 'Making People Laugh','Reading','Running',
+           'Play Badminton']
+          ]
+#Import libraries
+encodings, rev_encodings = venn.get_str_labels(values)
+set_labels = ['Get Paid For', 'World Needs', 'You Love', 'You are good at']
+
+fig, ax = venn.ikigai_venn4(dict(zip(set_labels,encodings.values())), names=set_labels, figsize=(16,16),
+                        rev_encs = rev_encodings,
+                        venn_type='circle', enc_type='encoding',)
+plt.title("My Ikigai");
+
+img_buf = io.BytesIO()
+#plt.savefig('ikigai.png', format='png')
+plt.show()
+
+#set_colors=('r', 'g','purple', 'skyblue'), alpha = 0.7);)
